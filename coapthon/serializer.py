f22f1eedfd5770d8fc8d2f953e648918748e32af
Denis Vasilkovskii <denis.vasilkovskii@tado.com>
2020-10-23 19:59:50 +0200

    fix(deserializer): Correct bytes slices convertion to ints

--- coapthon/serializer.py
+++ coapthon/serializer.py
@@ -27,6 +27,7 @@ class Serializer(object):
         :return: the message
         :rtype: Message
         """
+        logger.info(f"New datagram of type: {type(datagram)}")
         try:
             fmt = "!BBH"
             pos = struct.calcsize(fmt)
@@ -305,11 +306,12 @@ class Serializer(object):
         if h_nibble <= 12:
             value = h_nibble
         elif h_nibble == 13:
-            value = struct.unpack("!B", values[pos].to_bytes(1, "big"))[0] + 13
+            logger.info(f"Unpack longer H option: {values[pos:pos+1]}")
+            value = struct.unpack("!B", values[pos:pos+1])[0] + 13
             pos += 1
         elif h_nibble == 14:
-            s = struct.Struct("!H")
-            value = s.unpack_from(values[pos:].to_bytes(2, "big"))[0] + 269
+            logger.info(f"Unpack longest H option: {values[pos:pos+2]}")
+            value = struct.unpack("!H", values[pos:pos+2])[0] + 269
             pos += 2
         else:
             raise AttributeError("Unsupported option number nibble " + str(h_nibble))
@@ -317,10 +319,12 @@ class Serializer(object):
         if l_nibble <= 12:
             length = l_nibble
         elif l_nibble == 13:
-            length = struct.unpack("!B", values[pos].to_bytes(1, "big"))[0] + 13
+            logger.info(f"Unpack longer L option: {values[pos:pos+1]}")
+            length = struct.unpack("!B", values[pos:pos+1])[0] + 13
             pos += 1
         elif l_nibble == 14:
-            length = s.unpack_from(values[pos:].to_bytes(2, "big"))[0] + 269
+            logger.info(f"Unpack longest L option: {values[pos:pos+2]}")
+            length = struct.unpack("!H", values[pos:pos+2])[0] + 269
             pos += 2
         else:
             raise AttributeError("Unsupported option length nibble " + str(l_nibble))
